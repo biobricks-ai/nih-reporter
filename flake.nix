e4fa49703951adeb206d5fe16e302343d6e0e812
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-04-17 15:01:36 -0400

    feat(build): Add ExPORTER to brick
    For `/projects`, `/abstracts`, `/clinicalstudies`.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-2411.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-2411, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
         let
           pkgs = import nixpkgs {
+            inherit system;
+          };
+          pkgs-2411 = import nixpkgs-2411 {
             inherit system;
           };
       in {
@@ -20,8 +24,8 @@
           in pkgs.mkShell {
             buildInputs = [
               (python3.withPackages (ps: with ps; [ selenium webdriver-manager tqdm pandas pyarrow ]))
-              pkgs.chromium
-              pkgs.chromedriver
+              (with pkgs; [ chromium chromedriver ])
+              (with pkgs-2411; [ recode parallel coreutils duckdb ])
             ];
           };
       });
---
+++ stages/03_exporter_build.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+set -eu
+export LC_ALL=C
+
+export downloaddir='download'
+export rawdir='raw'
+export brickdir_exporter='brick/ExPORTER'
+
+{ [ -d "$rawdir" ] && rm -Rf "$rawdir"; }  || true
+mkdir -p "$rawdir"
+
+mkdir -p "$brickdir_exporter"
+
+
+# Exit function to clean up temporary directory
+function cleanup() {
+	echo "Cleaning up raw directory..."
+	rm -rf "$rawdir"
+	exit ${1:-0}
+}
+
+# Set trap to ensure cleanup on exit, interrupt, or error
+trap 'cleanup $?' EXIT INT TERM
+
+
+
+
+function process_by_group_prefix() {
+	export D_GROUP="$( echo $1 | cut -d/ -f1)"
+	export D_PREFIX="$(echo $1 | cut -d/ -f2)"
+	ls $downloaddir/${D_GROUP}/${D_PREFIX}_*.zip | parallel --bar '
+		outputdir=${rawdir}/${D_GROUP}/${D_PREFIX};
+		mkdir -p $outputdir
+		unzip -d ${outputdir} {}
+	'
+}
+
+function process_by_group_prefix_with_fy() {
+	export D_GROUP="$( echo $1 | cut -d/ -f1)"
+	export D_PREFIX="$(echo $1 | cut -d/ -f2)"
+	ls $downloaddir/${D_GROUP}/${D_PREFIX}_*.zip | parallel --bar '
+		FY={= ($_) = /FY(\d{4})/; =}
+		outputdir=${rawdir}/${D_GROUP}/${D_PREFIX}/FY=${FY};
+		mkdir -p $outputdir
+		unzip -d ${outputdir} {}
+	'
+}
+
+# ExPORTER/projects/RePORTER_PRJ_C {{{
+process_by_group_prefix projects/RePORTER_PRJ_C
+
+mkdir -p "$brickdir_exporter/projects"
+duckdb -c "$(cat <<'SQL'
+COPY (
+	SELECT
+		*
+	FROM read_csv(
+		'raw/projects/RePORTER_PRJ_C/*.csv',
+		encoding = 'utf-8',
+		header = true,
+		quote = '"',
+		escape = '"',
+		union_by_name = true
+	)
+) TO 'brick/ExPORTER/projects/RePORTER_PRJ_C.parquet' (FORMAT parquet, PARTITION_BY (FY));
+SQL
+)"
+# }}}
+
+# ExPORTER/projects/RePORTER_PRJFUNDING_C {{{
+process_by_group_prefix projects/RePORTER_PRJFUNDING_C
+
+mkdir -p "$brickdir_exporter/projects"
+duckdb -c "$(cat <<'SQL'
+COPY (
+	SELECT
+		*
+	FROM read_csv(
+		'raw/projects/RePORTER_PRJFUNDING_C/*.csv',
+		encoding = 'utf-8',
+		header = true,
+		quote = '"',
+		escape = '"',
+		union_by_name = true
+	)
+) TO 'brick/ExPORTER/projects/RePORTER_PRJFUNDING_C.parquet' (FORMAT parquet, PARTITION_BY (FY));
+SQL
+)"
+# }}}
+
+# ExPORTER/abstracts/RePORTER_PRJABS_C {{{
+process_by_group_prefix_with_fy abstracts/RePORTER_PRJABS_C
+
+#file ${rawdir}/abstracts/RePORTER_PRJABS_C/FY\=*/*.csv | grep -P 'ASCII|ISO-8859' | cut -d: -f1 | xargs -P$(nproc) recode -v ISO-8859-1..UTF-8
+#find ${rawdir}/abstracts/RePORTER_PRJABS_C/ -type f | xargs -P$(nproc)  recode -v ISO-8859-1..UTF-8
+find ${rawdir}/abstracts/RePORTER_PRJABS_C/ -type f -name '*.csv' | sort | parallel --bar -k '
+	FY={= ($_) = m</FY=(\d{4})/> =};
+	{ [ "$FY" -le 2021 ] && recode -v ISO-8859-1..UTF-8 {} ; } || true;
+'
+
+mkdir -p "$brickdir_exporter/abstracts"
+duckdb -c "$(cat <<'SQL'
+COPY (
+	SELECT
+		*
+	FROM read_csv(
+		'raw/abstracts/RePORTER_PRJABS_C/**/*.csv',
+		header = true,
+		strict_mode = false,
+		quote = '"',
+		escape = '"',
+		union_by_name = true,
+		hive_partitioning = true
+	)
+) TO 'brick/ExPORTER/abstracts/RePORTER_PRJABS_C.parquet' (FORMAT parquet, PARTITION_BY (FY));
+SQL
+)"
+# }}}
+
+# ExPORTER/clinicalstudies/ClinicalStudies {{{
+
+# read the single file directly
+mkdir -p "$brickdir_exporter/clinicalstudies"
+duckdb -c "$(cat <<'SQL'
+COPY (
+	SELECT
+		*
+	FROM read_csv(
+		'download/clinicalstudies/ClinicalStudies.csv',
+		header = true,
+		quote = '"',
+		escape = '"'
+	)
+) TO 'brick/ExPORTER/clinicalstudies/ClinicalStudies.parquet' (FORMAT parquet);
+SQL
+)"
+# }}}
+
+# vim: fdm=marker
